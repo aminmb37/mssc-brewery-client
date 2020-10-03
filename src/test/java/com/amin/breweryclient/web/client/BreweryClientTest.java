5995827f9c6cacd36dd39036b73b3141f5b02e27
Amin <amin.mb37@gmail.com>
2020-10-03 02:20:36 +0200

    Adding RestTemplate Client methods and testing them. Closes #2

---
+++ src/test/java/com/amin/breweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,72 @@
+package com.amin.breweryclient.web.client;
+
+import com.amin.breweryclient.web.model.BeerDto;
+import com.amin.breweryclient.web.model.CustomerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class BreweryClientTest {
+    @Autowired
+    private BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+        BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
+        assertNotNull(beerDto);
+    }
+
+    @Test
+    void saveNewBeer() {
+        //given
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        URI uri = breweryClient.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void updateBeer() {
+        //given
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        breweryClient.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void deleteBeer() {
+        breweryClient.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerDto customerDto = breweryClient.getCustomerById(UUID.randomUUID());
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void saveNewCustomer() {
+        //given
+        CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+        URI uri = breweryClient.saveNewCustomer(customerDto);
+        assertNotNull(uri);
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void updateCustomer() {
+        //given
+        CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+        breweryClient.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void deleteCustomer() {
+        breweryClient.deleteCustomer(UUID.randomUUID());
+    }
+}
